a9828ee68ff0a4e6784f65d357ada34b1e5bb011
Josh Fung <joshuagyfung@gmail.com>
2023-01-22 09:22:58 -0800

    Started MarketplaceCard

--- frontend/src/screens/marketplace/marketplace.js
+++ frontend/src/screens/marketplace/marketplace.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ItemShowcase from '../../components/ItemShowcase';
 import ProfileCard from '../../components/ProfileCard';
 import SideNav from '../../components/SideNav';
+import { Box } from '@mui/material';
 import './marketplace.css';
+import SearchIcon from '@mui/icons-material/Search';
 
 const Marketplace = () => {
     return(
@@ -12,10 +14,21 @@ const Marketplace = () => {
                 <ProfileCard username="Mode Demo" name="Jane"/>
             </div>
             <div className='marketplaceContent'> 
-                {/* <div> Test </div> */}
-                <ItemShowcase/>
+                {/* <ItemShowcase/> */}
+                <Box className="marketplace-upper-container">
+                    <Box className="marketplace-upper-tools">
+                        <Box className="marketplace-search">
+                            <SearchIcon style={{marginLeft: "5%"}}/>
+                            <div className="search-for-anything">Search for items...</div>
+                        </Box>
+                        <button className="marketplace-upload">UPLOAD</button>
+                    </Box>
+                    <div className="marketplace-title">Marketplace</div>
+                </Box>
+                <Box className="marketplace-lower-container">
+                    
+                </Box>
             </div>
-            {/* <h1>Marketplace</h1> */}
         </div>
     )
 }
