647679601f53c5e26b5debaf04cecf600e244ef7
Martin Tang <martintang8422@gmail.com>
2023-01-22 11:13:27 -0800

    Finished Cards

--- frontend/src/screens/marketplace/marketplace.js
+++ frontend/src/screens/marketplace/marketplace.js
@@ -6,12 +6,16 @@ import { Box } from "@mui/material";
 import "./marketplace.css";
 import SearchIcon from "@mui/icons-material/Search";
 import MarketplaceCard from "../../components/MarketplaceCard";
+
 import beigeShirt from "../../assets/beigeshirt.png";
-import blackDress from "../../assets/blackDress.png";
-import denimJacket from "../../assets/denimJacket.png";
+import blackDress from "../../assets/blackdress.png";
+import denimJacket from "../../assets/denimjacket.png";
 import purplePuffer from "../../assets/hoodedjacket.jpg";
 import blackCardigan from "../../assets/patterncardigan.png";
 import redShirt from "../../assets/redshirt.png";
+import sample_cardigan from "../../assets/sample_cardigan.jpg";
+import sweatshirt from "../../assets/sweatshirt.webp";
+import { NavLink } from "react-router-dom";
 
 const Marketplace = () => {
   const props = [
@@ -39,6 +43,30 @@ const Marketplace = () => {
       size: "Medium",
       image: purplePuffer,
     },
+    {
+      name: "Black Cardigan",
+      brand: "H&M",
+      size: "Small",
+      image: blackCardigan,
+    },
+    {
+      name: "Red Shirt",
+      brand: "Off-White",
+      size: "Large",
+      image: redShirt,
+    },
+    {
+      name: "Patterned Cardigan",
+      brand: "Garage",
+      size: "Medium",
+      image: sample_cardigan,
+    },
+    {
+      name: "Pink Sweatshirt",
+      brand: "Garage",
+      size: "Medium",
+      image: sweatshirt,
+    },
   ];
 
   return (
@@ -55,19 +83,23 @@ const Marketplace = () => {
               <SearchIcon style={{ marginLeft: "5%" }} />
               <div className="search-for-anything">Search for items...</div>
             </Box>
-            <button className="marketplace-upload">UPLOAD</button>
+            <div className="marketplace-upload">
+              <NavLink to="/upload" >
+                <button  className="marketplace-button">UPLOAD</button>
+              </NavLink>
+            </div>
           </Box>
           <div className="marketplace-title">Marketplace</div>
         </Box>
         <Box className="marketplace-items-container">
-          <MarketplaceCard {...props} />
-          <MarketplaceCard {...props} />
-          <MarketplaceCard {...props} />
-          <MarketplaceCard {...props} />
-          <MarketplaceCard {...props} />
-          <MarketplaceCard {...props} />
-          <MarketplaceCard {...props} />
-          <MarketplaceCard {...props} />
+          <MarketplaceCard {...props[0]} />
+          <MarketplaceCard {...props[1]} />
+          <MarketplaceCard {...props[2]} />
+          <MarketplaceCard {...props[3]} />
+          <MarketplaceCard {...props[4]} />
+          <MarketplaceCard {...props[5]} />
+          <MarketplaceCard {...props[6]} />
+          <MarketplaceCard {...props[7]} />
           {/* <div className="marketplace-item">
             <MarketplaceCard {...props} />
           </div>
