0522fe47f100d7153d1febf2fb756344c63b8186
Josh Fung <joshuagyfung@gmail.com>
2023-01-22 11:00:09 -0800

    crying for help

--- frontend/src/components/MarketplaceCard/index.js
+++ frontend/src/components/MarketplaceCard/index.js
@@ -1,25 +1,26 @@
-import React from 'react';
-import { Box } from '@mui/material';
+import React from "react";
+import { Box, CardMedia } from "@mui/material";
 import "./index.css";
+import test from "../../assets/beigeshirt.png";
 
 const MarketplaceCard = (props) => {
-  const 
+  const { brand, size, name, image } = props;
   return (
     <Box className="marketplace-card">
       <Box className="marketplace-card-image-container">
-
+        <img className="marketplace-item-image" src={image} alt="FUK" />
       </Box>
       <Box className="marketplace-card-info-container">
         <Box className="marketplace-card-brand-size">
-          <div className="marketplace-card-brand"></div>
-          <div className="marketplace-card-size"></div>
+          <div className="marketplace-card-brand">{brand}</div>
+          <div className="marketplace-card-size">{size[0]}</div>
         </Box>
         <Box className="marketplace-card-name-container">
-          <div className="marketplace-card-name"></div>
+          <div className="marketplace-card-name">{name}</div>
         </Box>
       </Box>
     </Box>
-  )
-}
+  );
+};
 
 export default MarketplaceCard;
--- frontend/src/screens/marketplace/marketplace.js
+++ frontend/src/screens/marketplace/marketplace.js
@@ -5,13 +5,47 @@ import SideNav from "../../components/SideNav";
 import { Box } from "@mui/material";
 import "./marketplace.css";
 import SearchIcon from "@mui/icons-material/Search";
+import MarketplaceCard from "../../components/MarketplaceCard";
+import beigeShirt from "../../assets/beigeshirt.png";
+import blackDress from "../../assets/blackDress.png";
+import denimJacket from "../../assets/denimJacket.png";
+import purplePuffer from "../../assets/hoodedjacket.jpg";
+import blackCardigan from "../../assets/patterncardigan.png";
+import redShirt from "../../assets/redshirt.png";
 
 const Marketplace = () => {
+  const props = [
+    {
+      name: "Plaid T-Shirt",
+      brand: "Uniqlo",
+      size: "Medium",
+      image: beigeShirt,
+    },
+    {
+      name: "Black Dress",
+      brand: "Aritzia",
+      size: "Small",
+      image: blackDress,
+    },
+    {
+      name: "Hooded Denim Jacket",
+      brand: "Levis",
+      size: "Large",
+      image: denimJacket,
+    },
+    {
+      name: "Purple Puffer",
+      brand: "Aritzia",
+      size: "Medium",
+      image: purplePuffer,
+    },
+  ];
+
   return (
     <div className="marketplacePage">
       <div className="marketplaceHeader">
         <SideNav />
-        <ProfileCard username="Mode Demo" name="Jane" />
+        {/* <ProfileCard username="Mode Demo" name="Jane" /> */}
       </div>
       <div className="marketplaceContent">
         {/* <ItemShowcase/> */}
@@ -24,16 +58,40 @@ const Marketplace = () => {
             <button className="marketplace-upload">UPLOAD</button>
           </Box>
           <div className="marketplace-title">Marketplace</div>
-          <div className="marketplace-items-container">
-            <div className="marketplace-item">.</div>
-            <div className="marketplace-item">.</div>
-            <div className="marketplace-item">.</div>
-            <div className="marketplace-item">.</div>
-            <div className="marketplace-item">.</div>
-            <div className="marketplace-item">.</div>
-            <div className="marketplace-item">.</div>
-            <div className="marketplace-item">.</div>
+        </Box>
+        <Box className="marketplace-items-container">
+          <MarketplaceCard {...props} />
+          <MarketplaceCard {...props} />
+          <MarketplaceCard {...props} />
+          <MarketplaceCard {...props} />
+          <MarketplaceCard {...props} />
+          <MarketplaceCard {...props} />
+          <MarketplaceCard {...props} />
+          <MarketplaceCard {...props} />
+          {/* <div className="marketplace-item">
+            <MarketplaceCard {...props} />
           </div>
+          <div className="marketplace-item">
+            <MarketplaceCard {...props} />
+          </div>
+          <div className="marketplace-item">
+            <MarketplaceCard {...props} />
+          </div>
+          <div className="marketplace-item">
+            <MarketplaceCard {...props} />
+          </div>
+          <div className="marketplace-item">
+            <MarketplaceCard {...props} />
+          </div>
+          <div className="marketplace-item">
+            <MarketplaceCard {...props} />
+          </div>
+          <div className="marketplace-item">
+            <MarketplaceCard {...props} />
+          </div>
+          <div className="marketplace-item">
+            <MarketplaceCard {...props} />
+          </div> */}
         </Box>
       </div>
     </div>
