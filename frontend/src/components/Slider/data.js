3e07116f48bccc3247b0c7b46b50cc59d20775af
Martin Tang <martintang8422@gmail.com>
2023-01-22 08:56:21 -0800

    Profile Change

--- frontend/src/components/Slider/data.js
+++ frontend/src/components/Slider/data.js
@@ -8,46 +8,47 @@
 // } from "@fortawesome/free-solid-svg-icons";
 
 import beigeshirt from "../../assets/beigeshirt.png";
-import redjacket from "../../assets/hoodedjacket.jpg";
-import denimjacket from "../../assets/denimjacket.png";
+import redjacket from "../../assets/redshirt.png";
+import denimjacket from "../../assets/sample_cardigan.jpg";
 import blackdress from "../../assets/blackdress.png";
 import patterncardigan from "../../assets/patterncardigan.png";
 
 export const data = [
   {
     id: 1,
-    bgColor: "#F54748",
-    image: <img alt="error" className="item-image" src={beigeshirt} />,
-    // image: "../../assets/beigeshirt.png",
-    title: "Calypso's Beige Shirt ",
-    desc: "Minted: Jan 23 2023",
+    bgColor: "#FF616D",
+    image: <img alt="error" className="item-image" src={redjacket} />,
+    title: "Jane's Red Shirt ",
+    desc: "Minted: Jan 21 2023",
   },
   {
     id: 2,
     bgColor: "#7952B3",
     image: <img alt="error" className="item-image" src={denimjacket} />,
-    title: "Calypso's Denim Jacket ",
-    desc: "Minted: Mar 17 2022",
+    title: "Jane's Sweater",
+    desc: "Minted: Jan 21 2023",
   },
   {
     id: 3,
     bgColor: "#1597BB",
     image: <img alt="error" className="item-image" src={blackdress} />,
-    title: "Calypso's Black Dress ",
-    desc: "Minted: Jun 27 2022",
+    title: "Jane's Black Dress",
+    desc: "Minted: Jan 21 2023",
   },
   {
     id: 4,
     bgColor: "#185ADB",
     image: <img alt="error" className="item-image" src={patterncardigan} />,
-    title: "Calypso's Pattern Cardigan",
-    desc: "Minted: Aug 8 2022",
+    title: "Jane's Pattern Cardigan",
+    desc: "Minted: Jan 21 2023",
   },
   {
     id: 5,
-    bgColor: "#FF616D",
-    image: <img alt="error" className="item-image" src={redjacket} />,
-    title: "Calypso's Maroon Jacket ",
-    desc: "Minted: Feb 29 2022",
-  },
+    bgColor: "#F54748",
+    image: <img alt="error" className="item-image" src={beigeshirt} />,
+    // image: "../../assets/beigeshirt.png",
+    title: "Jane's Beige Shirt ",
+    desc: "Minted: Jan 22 2023",
+  }
+
 ];
